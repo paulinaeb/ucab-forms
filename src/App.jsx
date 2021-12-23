c0170d7101ceb3e3312c5dd7b3af48f16c514bb3
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-12-22 22:06:48 -0400

    Installed MUI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Box>
+      <Typography variant="h1">UCAB Forms</Typography>
+    </Box>
+  );
+};
+
+export default App;
