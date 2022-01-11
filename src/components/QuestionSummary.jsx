6ff9284b9b27c91353c9e0d19ea0d04a4ba95b18
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2022-01-10 22:50:03 -0400

    Add responses summary

--- src/components/QuestionSummary.jsx
+++ src/components/QuestionSummary.jsx
@@ -1,6 +1,15 @@
 import { Box, Typography, Container } from "@mui/material";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  Title,
+  BarElement,
+} from "chart.js";
+import { Bar, Pie } from "react-chartjs-2";
 import {
   CHECKBOX,
   DATE,
@@ -13,7 +22,15 @@ import {
   TIME,
 } from "../constants/questions";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const QuestionSummary = ({ question, responses }) => {
   const numberOfResponses = responses.filter((r) => r[question.id]).length;
@@ -29,7 +46,7 @@ const QuestionSummary = ({ question, responses }) => {
       labels: question.options,
       datasets: [
         {
-          label: "# of Responses",
+          label: "Respuestas",
           data: question.options.map(
             (option) =>
               responses.filter((r) => r[question.id] === option).length
@@ -56,10 +73,91 @@ const QuestionSummary = ({ question, responses }) => {
     };
   }
 
+  if (question.type === CHECKBOX) {
+    data = {
+      labels: question.options,
+      datasets: [
+        {
+          label: "Respuestas",
+          data: question.options.map(
+            (option) =>
+              responses.filter((r) => r[question.id].includes(option)).length
+          ),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+        },
+      ],
+    };
+  }
+
+  if (question.type === SLIDER) {
+    const labels = [];
+
+    for (let i = question.min; i <= question.max; i++) {
+      labels.push(i);
+    }
+
+    data = {
+      labels,
+      datasets: [
+        {
+          label: "Respuestas",
+          data: labels.map(
+            (n) => responses.filter((r) => r[question.id] === n).length
+          ),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+        },
+      ],
+    };
+  }
+
   switch (question.type) {
     case TEXT:
-      return <Typography variant="body1">{numberOfResponsesText}</Typography>;
+    case TEXTAREA:
+    case DATE:
+    case TIME:
+    case DATETIME:
+      return (
+        <>
+          <Typography variant="body1">{numberOfResponsesText}</Typography>
+          {responses.map((r) => (
+            <Typography key={r.id} variant="body2">
+              {JSON.stringify(r[question.id])}
+            </Typography>
+          ))}
+        </>
+      );
     case RADIO:
+    case SELECT:
       return (
         <>
           <Typography variant="body1">{numberOfResponsesText}</Typography>
@@ -69,11 +167,62 @@ const QuestionSummary = ({ question, responses }) => {
         </>
       );
     case CHECKBOX:
-      return <Typography variant="body1">{numberOfResponsesText}</Typography>;
-    case TEXTAREA:
-      return <Typography variant="body1">{numberOfResponsesText}</Typography>;
-    case SELECT:
-      return <Typography variant="body1">{numberOfResponsesText}</Typography>;
+      return (
+        <>
+          <Typography variant="body1">{numberOfResponsesText}</Typography>
+          <Container maxWidth="sm">
+            <Bar
+              data={data}
+              options={{
+                indexAxis: "y",
+                elements: {
+                  bar: {
+                    borderWidth: 1,
+                  },
+                },
+                responsive: true,
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: true,
+                    text: question.title,
+                  },
+                },
+              }}
+            />
+          </Container>
+        </>
+      );
+    case SLIDER:
+      return (
+        <>
+          <Typography variant="body1">{numberOfResponsesText}</Typography>
+          <Container maxWidth="sm">
+            <Bar
+              data={data}
+              options={{
+                elements: {
+                  bar: {
+                    borderWidth: 1,
+                  },
+                },
+                responsive: true,
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  title: {
+                    display: true,
+                    text: question.title,
+                  },
+                },
+              }}
+            />
+          </Container>
+        </>
+      );
     default:
       return null;
   }
